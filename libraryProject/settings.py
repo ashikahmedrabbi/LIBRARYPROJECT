27f5494475f0412cb14d4d0c18e73878b0058cba
unknown <ashikahmedrabbi@gmail.com>
2024-03-07 19:39:03 +0600

    r

--- libraryProject/settings.py
+++ libraryProject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-10i=-+vuih-&zw2eco5hq&qh-3re4=viuu-zbzt5svb_+k*7ws
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
